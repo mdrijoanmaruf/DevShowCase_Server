b5d7441e07b205e04ed21e7fd07f582a7bcd3ad1
Md Rijoan Maruf <rmsaif4430@gmail.com>
2025-06-13 04:35:10 +0600

    Connected with database

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const prot = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
 app.use(cors());
 app.use(express.json());
 
@@ -12,3 +15,25 @@ app.get('/' , (req , res) => {
 app.listen(port , () => {
     console.log(`Server is running on port ${port}`);
 })
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0ykpaho.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+  }
+}
+run().catch(console.dir);
