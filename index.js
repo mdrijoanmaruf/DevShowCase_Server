a3ce9f61ab53a02ecda86872cae499f86a7cfbe7
Md Rijoan Maruf <rmsaif4430@gmail.com>
2025-06-13 04:25:34 +0600

    Basic Server setup

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const express = require('express');
+const cors = require('cors');
+const app = express();
+const prot = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
+
+app.get('/' , (req , res) => {
+    res.send('Welcome to the API');
+})
+
+app.listen(port , () => {
+    console.log(`Server is running on port ${port}`);
+})
